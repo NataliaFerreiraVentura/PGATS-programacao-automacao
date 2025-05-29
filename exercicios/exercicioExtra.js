57be00cf1d4b480765a9dc477c75c2e4c653f645
NataliaFerreiraVentura <nataliaferreiraventura@gmail.com>
2025-05-28 22:44:47 -0300

    Exercicio extra

---
+++ exercicios/exercicioExtra.js
@@ -0,0 +1,23 @@
+function filtrarNumeros(lista) {
+  return lista.filter(item => typeof item === 'number')
+}
+
+const entrada = [1, 'a', 3, 'b', 5, 'teste', 7]
+const resultado = filtrarNumeros(entrada)
+
+console.log(resultado)
+
+function contarVogais(texto) {
+  let contadorVogais = 0
+  const vogais = ['a', 'e', 'i', 'o', 'u']
+  const textoMinusculo = texto.toLowerCase()
+  for (let i = 0; i < textoMinusculo.length; i++) {
+    const caractere = texto[i]
+    if (vogais.includes(caractere)) {
+      contadorVogais++
+    }
+  }
+  return contadorVogais
+}
+console.log(contarVogais('Nathanael'))
+console.log(contarVogais('Naty'))
