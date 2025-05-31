1a7ee3896731aefe7a2db2623199ef4780a47367
NataliaFerreiraVentura <nataliaferreiraventura@gmail.com>
2025-05-31 00:34:30 -0300

    Adicioando conteudos teste de unidade

---
+++ testes/01-testes.js
@@ -0,0 +1,25 @@
+/**
+  describe --> agrupador de testes
+  it --> implementação do teste unitario
+
+  tdd
+  crio o teste
+  vejo o teste falhar
+  cria a implementacao para o teste passar
+  roda o teste de novo
+  refatoro o codigo para melhorar
+ */
+import { exibirNomeDogFormatado } from "../conceitos/teste-Unidade.js"
+import assert from 'node:assert'
+
+describe('Testes do projeto', () => {
+  it('Exibir o nome do dog com letrar maiusculas', () => {
+    assert.strictEqual(exibirNomeDogFormatado('Pantera'), 'PANTERA')
+    // verifique se valor atual >  igual ao valor esperado
+  })
+
+  it('segundo teste', () => {
+    throw new console.error()
+    
+  })
+})
