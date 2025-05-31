28df11b785bc779c54a7427d3f54e0be0e85c43c
NataliaFerreiraVentura <nataliaferreiraventura@gmail.com>
2025-05-31 14:58:53 -0300

    Subindo todos os conteudos aula 9

--- conceitos/async-await.js
+++ conceitos/async-await.js
@@ -1,3 +1,8 @@
+/*
+async: torna uma função assíncrona (sempre retorna uma Promise)
+
+await: pausa a execução até a Promise ser resolvida
+*/ 
 //async - assincrono
 async function exibirNomeDogFormatado(nome){
     return nome.toUpperCase()
